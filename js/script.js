2e03838224f48024dc42414d652157da5854f7a7
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-30 16:44:14 +0200

    javascript done

--- js/script.js
+++ js/script.js
@@ -2,17 +2,17 @@ console.log(`JS OK`);
 
 for( let i = 1 ; i <= 100 ; i++){
     let number 
-    if( i % 3 === 0 ){
+    if( i % 3 === 0 && i % 5 ===0 ){
+        number = `FizzBuzz`;
+        console.log(number);
+    }
+     else if( i % 3 === 0 ){
         
         number = `Fizz`;
         console.log(number);
     }
     else if( i % 5 === 0 ){
         number = `Buzz`;
-        console.log(number);
-    }
-    else if( i % 3 === 0 && i % 5 ===0 ){
-        number = `FizzBuzz`;
         console.log(number);
     }
     else{
