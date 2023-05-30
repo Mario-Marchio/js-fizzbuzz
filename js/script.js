2c9f0ff75a60d85a11944570cdc869d37cf4ea78
Mario-Marchio <mariomarchio1610@gmail.com>
2023-05-30 16:35:15 +0200

    js added

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-console.log(`JS OK`)
+console.log(`JS OK`);
 
+for( let i = 1 ; i <= 100 ; i++){
+    if( i % 3 === 0 ){
+        i = `Fizz`;
+        console.log(i);
+    }
+     else if( i % 5 === 0 ){
+        i = `Buzz`;
+        console.log(i);
+
+    }
+    else if( i % 3 === 0 && i % 5 ===0 ){
+        i = `FizzBuzz`;
+        console.log(i);
+    }
+    else{
+        console.log(i);
+    }
+}
